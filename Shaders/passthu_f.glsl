4dc29356ff23cee80335146d673119993044e953
EricLemieux <ericlemieux2@gmail.com>
2014-03-13 09:42:32 -0400

    Improved pass through shaders

--- Shaders/passthu_f.glsl
+++ Shaders/passthu_f.glsl
@@ -1,4 +1,12 @@
 #version 330 core
+
+in vertex
+{
+	vec3 pos;
+	vec3 norm;
+	vec2 UV;
+}data;
+
 void main()
 {
 	gl_FragColor = vec4(1.0, 0.5, 0.0, 1.0);
--- Shaders/passthu_v.glsl
+++ Shaders/passthu_v.glsl
@@ -1,11 +1,23 @@
 #version 330 core
 
-in vec4 position;
+layout (location=0) in vec4 position;
+layout (location=2) in vec3 normal;
+layout (location=8) in vec2 texcoord;
 
 uniform mat4 mvp;
 
+out vertex
+{
+	vec3 pos;
+	vec3 norm;
+	vec2 UV;
+}data;
 
 void main()
 {
 	gl_Position = mvp * position;
+
+	data.pos	= position.xyz;
+	data.norm	= normal;
+	data.UV		= texcoord;
 }
